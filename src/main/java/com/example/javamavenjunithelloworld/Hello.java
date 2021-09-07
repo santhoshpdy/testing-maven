ed6a8e3d7bce1ea1ffd00afb8650d1c4c598c36b
sandeep-Sandymn <87719318+sandeep-Sandymn@users.noreply.github.com>
2021-09-07 23:52:11 +0530

    Update Hello.java

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -1,50 +1,5 @@
-package com.example.javamavenjunithelloworld;
-
-import java.io.PrintStream;
-import java.lang.Exception;
-
-/**
- * Simple class that says "Hello!".
- */
-public class Hello {
-
-    static final String HELLO = "Hello!";
-
-    public static final int MAXIMUM_AMOUNT_OF_TIMES = 20;
-
-    private int times = 1;
-    
-    public int newint;
-    
-    public int secondint;
-
-
-    /**
-     * Set how many times "Hello!" should be said.
-     *
-     * @param times How many times should this class say "Hello!"? The value should be no larger than 20.
-     * @throws IllegalArgumentException Thrown when times is larger than 20 or a negative number.
-     */
-    public void setTimes(int times) {
-        if (times < 0 || times > MAXIMUM_AMOUNT_OF_TIMES) {
-            throw new IllegalArgumentException(String.format(
-                    "Parameter «times» should be a positive number no larger than %d.",
-                    MAXIMUM_AMOUNT_OF_TIMES
-            ));
-        }
-        this.times = times;
-        
+class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
     }
-
-    /**
-     * Say "Hello!".
-     *
-     * @param printer PrintStream to write output to.
-     */
-    public void sayHello(PrintStream printer) {
-        for (short i = 0; i < times; i++) {
-            printer.println(HELLO);
-        }
-    }
-
 }
